3e47172146ecfbfb81f01169bad4138e0e752ac3
Emma HB <emmanuelle.bolognini@gmail.com>
2021-10-08 17:17:42 +0200

    Collisions management, Score, Responsive settings

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+let W = window.innerWidth;
+let H = window.innerHeight;
+let score = 0;
+
 //THE GAME AREA
 const myGameArea = {
   
@@ -5,8 +9,8 @@ const myGameArea = {
   frames: 0, 
 
   start: function () {
-    this.canvas.width = 1000;
-    this.canvas.height = 600;
+    this.canvas.width = W; 
+    this.canvas.height = H; 
     this.context = this.canvas.getContext('2d');
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		this.interval = setInterval(updateGameArea, 16); //60 FPS
@@ -21,10 +25,18 @@ const myGameArea = {
 	clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
-};
 
-//const W = myGameArea.canvas.width;
-//const H = myGameArea.canvas.height;
+  printScore: function () {
+    this.context.font = '32px Ruslan Display, cursive';
+    this.context.fillStyle = 'yellow';
+    this.context.textAlign = "center";
+    if (score < 10) {
+    this.context.fillText(`0${score}`, W - 35, H - 25);
+    } else {
+      this.context.fillText(`${score}`, W - 35, H - 25);
+    }
+  }   
+};
 
 // COMPONENTS
 //Testing the Component class with rectangles before importing images
@@ -52,44 +64,45 @@ class Component {
   moveRight() {
     if (this.x < (myGameArea.canvas.width - this.width)) this.x += 30;
   }
+
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+ 
+  crashWith(el) {
+    return (
+      this.bottom() > el.top() &&
+      this.top() < el.bottom() &&
+      this.right() > el.left() &&
+      this.left() < el.right() &&
+      this.bottom() > el.bottom()
+    );
+  }
 }
 
 //The Wooden Raft
-const woodenRaft = new Component('darkred',((1000 / 2) - 75), 600 - 50, 175, 20); //(color, x: (W/2)- (this.width/2), y: H - 50, width, height)
+const woodenRaft = new Component('darkred',(W/2 - 90), H - 50, 180, 20); //(color, x: (W/2)- (this.width/2), y: H - 50, width, height)
 
 //Bush reeds
-const bushReedsLeft = new Component('greenyellow', 0, 600 - 175, 125, 175);
-const bushReedsRight = new Component('greenyellow', 1000 - 125, 600 - 175, 125, 175);
+const bushReedsLeft = new Component('greenyellow', 0, H - 175, 125, 175);
+const bushReedsRight = new Component('greenyellow', W - 125, H - 175, 125, 175);
 
-//Random Crocodiles
-/*const crocodile1 = new Component('green', 1000 / 5, 325, 75, 10);
-const crocodile2 = new Component('green', 1000 - 300, 375, 100, 15);
-const crocodile3 = new Component('green', 1000 / 3, 600- 50, 50, 50); //(x, y: 600 - heigth, width, heigth)
+//COMPONENTS with Random Positions
+const crocodile1 = new Component('green', W / 5, 325, 75, 10);
+const crocodile2 = new Component('green', W - 300, 375, 100, 15);
+const crocodile3 = new Component('green', W / 3, H- 40, 50, 50);
 
-const crocodiles = [crocodile1, crocodile2, crocodile3]; //array objects
-
-function random(arr) {
-  var randomIndex = Math.trunc (Math.random() * crocodiles.length);
-    return arr[randomIndex];
-}
-console.log(random(crocodiles)) //Component { color: 'green', x: 700, y: 375, width: 100, height: 15 }
-
-if (frames % 150 === 0) {
-  var crocodile = random(crocodiles);
-}
-console.log(crocodile) //undefined
-*/
-
-const crocodile1 = new Component('green', 1000 / 5, 325, 75, 10);
-const crocodile2 = new Component('green', 1000 - 300, 375, 100, 15);
-const crocodile3 = new Component('green', 1000 / 3, 600- 40, 50, 50); //(x, y: 600 - heigth, width, heigth)
-
-const crocodiles = [crocodile1, crocodile2, crocodile3]; //array objects
+const crocodiles = [crocodile1, crocodile2, crocodile3];
 let crocodile;
-
-const openMouthCroco = new Component('green', 1000 - 75, 600 - 100, 50, 100); //(x: this.x of Paratrooper, y: 600 - heigth, width, heigth)
-
-//Parachutes : Paratroopers & others
 
 function random(arr) {
   var randomIndex = Math.trunc (Math.random() * arr.length);
@@ -99,11 +112,17 @@ function random(arr) {
 const framesNumber = 800; 
 const speed = 1; // Increases by 1 each time the score reaches a new 10
 
-const myOrangeParatroopers = [];
-const myBrownParatroopers = [];
+let myOrangeParatroopers = [];
+let myBrownParatroopers = [];
 
 function updateRandomElements() {
   myGameArea.frames += 1;
+
+  if (myGameArea.frames % 200 === 0) {
+    crocodile = random(crocodiles);
+  }
+  crocodile.update();
+
   if (myGameArea.frames % (framesNumber/2) === 0) {
     var W = myGameArea.canvas.width;
     var minPosX = 0;
@@ -111,7 +130,7 @@ function updateRandomElements() {
 
     var orangePosX = Math.floor(Math.random() * (maxPosX - minPosX + 1) + minPosX);
 
-    myOrangeParatroopers.push(new Component('orange', orangePosX, 0, 50 , 60)); //color,x ,y, width, height
+    myOrangeParatroopers.push(new Component('orange', orangePosX, 0, 50 , 60)); 
   }
 
   for (i = 0; i < myOrangeParatroopers.length; i++) {
@@ -132,12 +151,7 @@ function updateRandomElements() {
   for (i = 0; i < myBrownParatroopers.length; i++) {
     myBrownParatroopers[i].y += speed * 1.5;
     myBrownParatroopers[i].update();
-  }
-  
-  if (myGameArea.frames % 200 === 0) {
-    crocodile = random(crocodiles);
-  }
-
+  } 
 }
 
 //MOTIONS
@@ -153,6 +167,32 @@ document.addEventListener('keydown', (e) => {
   }
 });
 
+//SAVED 
+let scoreboard = new Component ('green', W - 65, H - 55, 60, 40);
+
+function pickUp() {
+  myOrangeParatroopers = myOrangeParatroopers.filter(function(el) {
+    if (woodenRaft.crashWith(el)) {
+      score += 1; 
+      return false; 
+    } else {
+      return true; 
+    } 
+  })
+
+  myBrownParatroopers = myBrownParatroopers.filter(function(el) {
+    if (woodenRaft.crashWith(el)) {
+      score += 1;
+      return false; 
+    } else {
+      return true; 
+    }  
+  })
+}
+
+//GAME OVER
+//const openMouthCroco = new Component('green', W - 75, H - 100, 50, 100); //(x: this.x of Paratrooper, y: H - heigth, width, heigth)
+
 //Animation
 function updateGameArea() {
   //
@@ -160,21 +200,20 @@ function updateGameArea() {
   //
   myGameArea.clear(); //clear
 
-  woodenRaft.update();//draw woodenRaft
+  pickUp();
 
-  /*crocodile1.update();
-  crocodile2.update();
-  crocodile3.update();*/
+  woodenRaft.update();//draw woodenRaft
 
   updateRandomElements();
 
   bushReedsLeft.update();
   bushReedsRight.update();
 
-  crocodile.update();
+  scoreboard.update();
+  myGameArea.printScore();
 
   //GameOver = if y(bottom of paratrooper) > y(top of woodenRaft) && !collision between paratrooper and woodenRaft
-  openMouthCroco.update(); 
+  //openMouthCroco.update(); 
 }
 
 myGameArea.start();
